770399231986f9f3f39d5e7c8f968ca2837d4f5b
RioMorll <al415556@uji.es>
2023-02-16 10:45:27 +0100

    Version 4

--- statistics/arith_mean.cc
+++ statistics/arith_mean.cc
@@ -1,9 +1,13 @@
 #include <iostream>
 int main(int argc, char** argv){
     double m = 0, s = 0;
-    int n = argc -1;
-    for(int i=1;i<=n;i++)
-        s+= atoi(argv[i]);
+    int n = 0, element;
+    std::cin >> element;
+    while(element>0){
+        n+=1;
+        s+=element;
+        std::cin >>element;
+    }
     m = s/n;
     std::cout << "Arithmetic mean: " << m << std::endl;
 
