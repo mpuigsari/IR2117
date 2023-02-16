c057080b4a8128e3b132dedc2e296bb26bcd7e1c
RioMorll <al415556@uji.es>
2023-02-16 10:46:03 +0100

    Version 5

--- statistics/arith_mean.cc
+++ statistics/arith_mean.cc
@@ -3,7 +3,7 @@ int main(int argc, char** argv){
     double m = 0, s = 0;
     int n = 0, element;
     std::cin >> element;
-    while(element>0){
+    while(!std::cin.eof()){
         n+=1;
         s+=element;
         std::cin >>element;
