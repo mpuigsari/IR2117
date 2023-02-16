76cfd0b121fe9171388653f8a70229a1d363f033
RioMorll <al415556@uji.es>
2023-02-16 10:43:34 +0100

    Version 3

--- statistics/arith_mean.cc
+++ statistics/arith_mean.cc
@@ -3,7 +3,7 @@ int main(int argc, char** argv){
     double m = 0, s = 0;
     int n = argc -1;
     for(int i=1;i<=n;i++)
-        s+= argv[i];
+        s+= atoi(argv[i]);
     m = s/n;
     std::cout << "Arithmetic mean: " << m << std::endl;
 
