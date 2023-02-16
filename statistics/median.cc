4438d2f5a377bcae29f616b07ddbdb808220eda8
RioMorll <al415556@uji.es>
2023-02-16 18:25:23 +0100

    Median Version 2

--- statistics/median.cc
+++ statistics/median.cc
@@ -6,13 +6,17 @@ int main(int argc, char** argv){
     double m = 0, s = 0;
     int n = 0, element;
     std::vector<int> elements;
-    std::sort(elements.begin(), elements.end());
     std::cin >> element;
     while(not std::cin.eof()){
         elements.push_back(element);
         n+=1;
         std::cin >>element;
     }
+    std::sort(elements.begin(), elements.end());
+    if(elements.size()%2!=0)
+        m=0;
+    else
+        m=0;
     std::cout << "Median: " << m << std::endl;
 
     return 0;
