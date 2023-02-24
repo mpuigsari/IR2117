f75184f4755acfbb5934064f9163ffd8a6538e98
RioMorll <al415556@uji.es>
2023-02-24 14:35:34 +0100

    Mode - Version 2 - Count
    Se modifica el vector a array 1D para asi poder emplear la función count que permite contar cuantas veces aparecen expresiones booleanas

--- Matrices/mode.cc
+++ Matrices/mode.cc
@@ -2,18 +2,15 @@
 #include <eigen3/Eigen/Dense>
 #include "utils.hpp"
 int main(int argc, char** argv){
-    Eigen::VectorXi elements = ReadElements();
+    Eigen::ArrayXXi elements = ReadElements();
     double m = 0;
-    int n = elements.size();
-    int count[n], max= 0;
+    int n = elements.cols();
+    int count,max= 0;
     for(int i=0; i<n; i++){
-        count[i]=0;
-        for(int j = 0; j<n; j++){
-            if(elements[i] == elements[j])
-                count[i]++;
-            if(count[i]>count[max])
-                max=i;}}
-    m = elements[max];
+        count= (elements == elements[0,i]).count();
+        if(count > max){
+            max=count;
+            m = elements[0,i];}}
     std::cout << "Mode: " << m << std::endl;
 
     return 0;
