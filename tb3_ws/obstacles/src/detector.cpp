2553f208b41a3bb9391c817633f1468a0df64472
RioMorll <al415556@uji.es>
2023-04-04 18:05:00 +0200

    obstacle_detector - Version 3

--- tb3_ws/obstacles/src/detector.cpp
+++ tb3_ws/obstacles/src/detector.cpp
@@ -4,10 +4,24 @@
 #include "example_interfaces/msg/bool.hpp"
 
 std::shared_ptr<rclcpp::Publisher<example_interfaces::msg::Bool>> publisher;
+double obs_angle_min = -M_PI/8, obs_angle_max = M_PI/8, obs_threshold = 1.0;
 
 void topic_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg){
     example_interfaces::msg::Bool out_msg;
     out_msg.data = false;
+    
+    float angle = msg->angle_min;
+    for(float range: msg->ranges){
+        if(angle > M_PI) 
+            angle-=2*M_PI;
+        if((angle >=obs_angle_min) and (angle <= obs_angle_max)){
+            if(range<= obs_threshold){
+                out_msg.data = true;
+                break;
+            }
+        }
+        angle += msg->angle_increment;
+    }
     publisher->publish(out_msg);
 }
 
@@ -16,6 +30,13 @@ int main(int argc, char * argv[]){
     auto node = rclcpp::Node::make_shared("detector");
     auto subscription = node->create_subscription<sensor_msgs::msg::LaserScan>("/scan",10,topic_callback);
     publisher = node->create_publisher<example_interfaces::msg::Bool>("obstacle", 10);
+    node->declare_parameter("obs_angle_min", -M_PI/8);
+    node->declare_parameter("obs_angle_max", M_PI/8);
+    node->declare_parameter("obs_threshold", 1.0);
+    obs_angle_min = node->get_parameter("obs_angle_min").get_parameter_value().get<double>();
+    obs_angle_max = node->get_parameter("obs_angle_max").get_parameter_value().get<double>();
+    obs_threshold = node->get_parameter("obs_threshold").get_parameter_value().get<double>();
+    
     rclcpp::spin(node);
     rclcpp::shutdown();
     return(0);
