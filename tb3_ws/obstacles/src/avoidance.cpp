ff61fc82c0dca9ac939f7e568bd542427d83371d
RioMorll <al415556@uji.es>
2023-04-19 17:02:25 +0200

    avoidance version gazeebo

--- tb3_ws/obstacles/src/avoidance.cpp
+++ tb3_ws/obstacles/src/avoidance.cpp
@@ -5,21 +5,18 @@
 #include <stdlib.h>
 
 using namespace std::chrono_literals;
-bool izq,der,fr, mov, turn_left, turn_right, rd;
+bool izq,der,fr;
 
 void callback_front(const example_interfaces::msg::Bool::SharedPtr msg){
-     if(msg)
-         rd = true;
+     fr = msg->data;
 }
 
 void callback_left(const example_interfaces::msg::Bool::SharedPtr msg){
-    if(msg)
-         turn_left = true;
+    izq = msg->data;
 }
 
 void callback_right(const example_interfaces::msg::Bool::SharedPtr msg){
-    if(msg)
-         turn_right = true;
+    der = msg->data;
 }
 
 int main(int argc, char *argv[]){
@@ -30,26 +27,32 @@ int main(int argc, char *argv[]){
     auto subs_left = node->create_subscription<example_interfaces::msg::Bool>("/left/obstacle", 10,callback_left);
     auto subs_right = node->create_subscription<example_interfaces::msg::Bool>("/right/obstacle", 10,callback_right);
     int randint;
+    bool turn_left, turn_right,rd;
     geometry_msgs::msg::Twist message;
-    rclcpp::WallRate loop_rate(50ms);
+    rclcpp::WallRate loop_rate(100ms);
     
     while(rclcpp::ok()){
-        mov = (turn_right or turn_left or rd);
-        switch(mov){
-            case true:
-                if(rd)
-                    randint = rand() %10+1;
-                message.linear.x = 0.0;
-                break;
-            case false:
-                turn_left = false, turn_right=false, rd = false;
-                message.linear.x = 1.0;
-                message.angular.z = 0.0;
-                break;
+        rd = false, turn_left = false, turn_right = false;
+        if(fr){
+            message.linear.x = 0.0;
+            if(der)
+                turn_left = true;
+            else {
+                if(izq)
+                    turn_right = true;
+                else{
+                    rd = true;
+                    randint = rand() %10+1;}
+            }
         }
-        if(turn_left or (rd and randint <= 5))
+        else{
+            message.linear.x = 0.5;
+            message.angular.z = 0.0;
+        }
+        
+        if(turn_left or (rd and randint >= 5))
             message.angular.z = 0.157;
-        if(turn_right or (rd and randint > 5))
+        if(turn_right or (rd and randint < 5))
             message.angular.z = -0.157;
         
         publisher->publish(message);
