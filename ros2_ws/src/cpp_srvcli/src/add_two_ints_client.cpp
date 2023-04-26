c0d92a095bd063349b65a45786d57fc11b353756
RioMorll <al415556@uji.es>
2023-04-26 15:47:14 +0200

    SRVCLI - VERSION 3.1
    Add Executable

--- ros2_ws/src/cpp_srvcli/src/add_two_ints_client.cpp
+++ ros2_ws/src/cpp_srvcli/src/add_two_ints_client.cpp
@@ -14,12 +14,9 @@ int main(int argc, char **argv)
      "usage: add_two_ints_client X Y");
   	return 1;
   }
-  std::shared_ptr<rclcpp::Node> node = 
-    rclcpp::Node::make_shared("add_two_ints_client");
-  rclcpp::Client<AddTwoInts>::SharedPtr client =
-    node->create_client<AddTwoInts>("add_two_ints");
-  auto request =
-    std::make_shared<AddTwoInts::Request>();
+  std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("add_two_ints_client");
+  rclcpp::Client<AddTwoInts>::SharedPtr client = node->create_client<AddTwoInts>("add_two_ints");
+  auto request = std::make_shared<AddTwoInts::Request>();
   request->a = atoll(argv[1]);
   request->b = atoll(argv[2]);
 
