09330d3da6ccdd76a15a88e6ed0eefed59643c38
RioMorll <al415556@uji.es>
2023-04-28 18:40:09 +0200

    Rings Version 5 Services

--- tb3_ws/olympic/src/rings.cpp
+++ tb3_ws/olympic/src/rings.cpp
@@ -5,9 +5,11 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 #include "turtlesim/srv/set_pen.hpp"
+#include "turtlesim/srv/teleport_relative.hpp"
 
 using namespace std::chrono_literals;
 using turtlesim::srv::SetPen;
+using turtlesim::srv::TeleportRelative;
 
 int service_pen(std::shared_ptr<rclcpp::Node> node , int red, int green, int blue, int w){
  rclcpp::Client<SetPen>::SharedPtr client = node->create_client<SetPen>("/turtle1/set_pen");
@@ -17,7 +19,7 @@ int service_pen(std::shared_ptr<rclcpp::Node> node , int red, int green, int blu
  request->g = green;
  request->b = blue;
  request->width = w;
- while (!client->wait_for_service(1s)) {
+ while (!client->wait_for_service(500ms)) {
     if (!rclcpp::ok()) {
       RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), 
        "Interrupted while waiting for the service.");
@@ -43,6 +45,39 @@ int service_pen(std::shared_ptr<rclcpp::Node> node , int red, int green, int blu
     
 }
 
+int service_tp(std::shared_ptr<rclcpp::Node> node , float l, float a){
+ rclcpp::Client<TeleportRelative>::SharedPtr client = node->create_client<TeleportRelative>("/turtle1/teleport_relative");
+    
+ auto request = std::make_shared<TeleportRelative::Request>();
+ request->linear = l;
+ request->angular = a;
+
+ while (!client->wait_for_service(500ms)) {
+    if (!rclcpp::ok()) {
+      RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), 
+       "Interrupted while waiting for the service.");
+      return 1;
+	 }
+	 RCLCPP_INFO(rclcpp::get_logger("rclcpp"), 
+     "service not available, waiting again...");
+  }
+    auto result = client->async_send_request(request);
+
+  if (rclcpp::spin_until_future_complete(node, 
+       result) ==	rclcpp::FutureReturnCode::SUCCESS)
+  {
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), 
+     "Success teleport_relative");
+    
+  } else {
+    RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), 
+     "Failed to call service teleport_relative");
+    return 1;
+  }
+ return 0;
+    
+}
+
 int main(int argc, char * argv[])
 {
  rclcpp::init(argc, argv);
@@ -51,16 +86,13 @@ int main(int argc, char * argv[])
  node->declare_parameter("radius", 1.0);
  geometry_msgs::msg::Twist vel;
  rclcpp::WallRate loop_rate(20ms);
- 
-int pen = service_pen(node,255,255,0,5);
-if(pen == 1){
- rclcpp::shutdown();
- return 1;
-}
+ int pen,tp;
+
  
  double radius = node->get_parameter("radius").get_parameter_value().get<double>();
  while (rclcpp::ok()) {
-    for(int i = 0; i < 50 ; i++){
+     pen = service_pen(node,255,255,0,5);
+     for(int i = 0; i < 50 ; i++){
         vel.angular.z = 2* M_PI;
         vel.linear.x = 2* M_PI*radius;
         publisher->publish(vel);
@@ -72,6 +104,10 @@ if(pen == 1){
     publisher->publish(vel);
     rclcpp::spin_some(node);
     loop_rate.sleep();
+    tp = service_tp(node,radius*1.08,0);
+    if(tp == 1 or pen == 1){
+        rclcpp::shutdown();
+        return 1;}
     break;
 }
  rclcpp::shutdown();
